3799df8ee62afbca9306fe758e676cd381536adf
Bruce chen <cqdeyouxiang@163.com>
2015-09-04 21:58:13 +0800

    Initial Commit

---
+++ FactoryPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FactoryPattern
+//
+//  Created by Bruce on 15/9/4.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
